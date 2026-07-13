01a7550bace5030bb6103a6ba76708ec6ab0dabe
Santeri Louna <santeri.louna@hotmail.fi>
2026-07-13 20:56:42 +0300

    kahden saman nimen lisäys eri kirjain koolla estetty, 2.11 json database

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 //TODO: vain yksi lista näkyvissä, refaktorointi
 
@@ -10,12 +11,19 @@ const App = () => {
 
   
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
   const search = (event) => {
     console.log(event.target.value)
@@ -26,7 +34,7 @@ const App = () => {
   const addPerson = (event) =>{
     event.preventDefault()
     for(let i = 0; i < persons.length; i++){
-      if(newName === persons[i].name){
+      if(newName.toLowerCase() === persons[i].name.toLowerCase()){
         alert(`${newName} is already added`)
         break;
         
