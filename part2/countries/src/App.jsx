a5c8e3d9cb9951bdd82be2a2567e607c5f76f246
Santeri Louna <santeri.louna@hotmail.fi>
2026-08-06 12:24:37 +0300

    2.18

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from 'react'
+import countryService from './services/countries'
+
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [searchTerms, setSearchTerms] = useState('')
+
+  useEffect(() => {
+      countryService
+        .getAll()
+        .then(response => {
+          setCountries(response.data)
+        })
+    }, [])
+
+    const search = (event) => {
+    console.log(event.target.value)
+    event.preventDefault()
+    setSearchTerms(event.target.value)
+  
+  }
+  
+
+
+  return (
+    <div>
+    <div>Jello</div>
+    <Filter countries = {countries} onChange = {search} value = {searchTerms} />
+    <Countries countries = {countries} searchTerms= {searchTerms} />
+    </div>
+
+  )
+}
+
+const Filter = (props) =>{
+
+  return(
+    <div >
+      <form onChange={props.onChange} onSubmit={e => { e.preventDefault(); }}>
+        <div>
+          Type to filter: <input value={props.value}
+          onChange={props.onChange} />
+          </div> 
+      </form>
+    </div>
+    
+  )
+}
+
+const Countries = (props) => {
+  const filtered = props.countries.filter((country) => 
+      country.name.common.toLowerCase().includes(props.searchTerms.toLowerCase()))
+
+  if(filtered.length < 10){
+    return(
+    <div>
+      {filtered.map(country =>
+        <div key={country.name.common}>{country.name.common} </div>)}
+    </div>
+  )
+  } else {
+    return(
+      <div>Too many matches, please specify</div>
+    )
+  }
+  
+}
+
+export default App
